878413b146a2033dbc77db8e9cfa4c5c506dfed2
raulsuper30 <rauldediego30@gmail.com>
2023-04-23 22:33:42 +0200

    Implementar comprobación del sistema operativo para permitir la ejecución solo en Windows

--- src/main/java/com/facturpdr/aplicacion/Inicio.java
+++ src/main/java/com/facturpdr/aplicacion/Inicio.java
@@ -1,12 +1,19 @@
 package com.facturpdr.aplicacion;
 
 import com.facturpdr.aplicacion.general.extensiones.VentanaExtension;
+import com.facturpdr.aplicacion.general.utilidades.AlertaUtilidad;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
 public class Inicio extends Application {
     @Override
     public void start(Stage escenarioPrincipal) {
+        String sistema = System.getProperty("os.name");
+        if (!sistema.startsWith("Windows")) {
+            AlertaUtilidad.error("Error de sistema operativo", "La aplicación solo se puede ejecutar en un sistema operativo Windows");
+            return;
+        }
+
         VentanaExtension ventana = VentanaExtension.obtenerInstancia();
 
         ventana.cambiarEscena("auth/iniciar-sesion.fxml");
