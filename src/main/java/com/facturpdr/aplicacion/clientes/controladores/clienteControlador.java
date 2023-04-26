f4b8ef1fe2947c2d86395de250d5485b6072caab
Nicoooo26 <121027723+Nicoooo26@users.noreply.github.com>
2023-04-26 23:02:32 +0200

    Update clienteControlador.java

--- src/main/java/com/facturpdr/aplicacion/clientes/controladores/clienteControlador.java
+++ src/main/java/com/facturpdr/aplicacion/clientes/controladores/clienteControlador.java
@@ -3,7 +3,6 @@ package com.facturpdr.aplicacion.clientes.controladores;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.control.TableView;
 
 public class clienteControlador {
 
