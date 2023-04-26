2d1052c1cc68a6b089416f0efce6a540f4271a05
Nicoooo26 <nicogpfutbol@gmail.com>
2023-04-26 12:16:15 +0200

    modelo cliente

--- src/main/java/com/facturpdr/aplicacion/clientes/controladores/clienteControlador.java
+++ src/main/java/com/facturpdr/aplicacion/clientes/controladores/clienteControlador.java
@@ -3,8 +3,10 @@ package com.facturpdr.aplicacion.clientes.controladores;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
 
 public class clienteControlador {
+
 
     @FXML
     public Button btnBack;
--- src/main/java/com/facturpdr/aplicacion/clientes/controladores/clientesControlador.java
+++ src/main/java/com/facturpdr/aplicacion/clientes/controladores/clientesControlador.java
@@ -3,10 +3,25 @@ package com.facturpdr.aplicacion.clientes.controladores;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 
 public class clientesControlador {
 
+    @FXML
+    public TableView tablaClientes;
+    @FXML
+    public TableColumn columnaFoto;
+    @FXML
+    public TableColumn columnaDNI;
+    @FXML
+    public TableColumn columnaNombre;
+    @FXML
+    public TableColumn columnaTelefono;
+    @FXML
+    public TableColumn columnaCuenta;
+    
     @FXML
     public TextField textBuscar;
     @FXML
