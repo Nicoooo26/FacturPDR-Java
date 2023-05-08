513f585393b7fbd28818ece2b66074ee70eab15b
raulsuper30 <rauldediego30@gmail.com>
2023-05-08 12:07:00 +0200

    Refactorizacion de método eliminarPreferencias

--- src/main/java/com/facturpdr/aplicacion/sesiones/utilidades/ConfiguracionUtilidad.java
+++ src/main/java/com/facturpdr/aplicacion/sesiones/utilidades/ConfiguracionUtilidad.java
@@ -4,22 +4,28 @@ import java.util.prefs.BackingStoreException;
 import java.util.prefs.Preferences;
 
 public class ConfiguracionUtilidad {
-    private static final String PREFERENCIAS_NODO_NOMBRE = "com.facturpdr.aplicacion";
+    private static final String NODO_NOMBRE = "com.facturpdr.aplicacion";
     private static final String JWT_TOKEN_KEY = "jwt_token";
-    private static final Preferences preferencias = Preferences.userRoot().node(PREFERENCIAS_NODO_NOMBRE);;
+    private static final Preferences nodo = Preferences.userRoot().node(NODO_NOMBRE);;
 
     public static void eliminarPrefencias() {
         try {
-            preferencias.removeNode();
+            String valor = nodo.get(NODO_NOMBRE, null);
+            if (valor != null) {
+                String[] subnodos = nodo.childrenNames();
+                for (String subnodo : subnodos) {
+                    nodo.node(subnodo).removeNode();
+                }
+            }
         } catch (BackingStoreException ignored) { }
     }
     public static String obtenerJWT() {
-        return preferencias.get(JWT_TOKEN_KEY, null);
+        return nodo.get(JWT_TOKEN_KEY, null);
     }
     public static void establecerJWT(String token) {
-        preferencias.put(JWT_TOKEN_KEY, token);
+        nodo.put(JWT_TOKEN_KEY, token);
     }
     public static void eliminarJWT() {
-        preferencias.remove(JWT_TOKEN_KEY);
+        nodo.remove(JWT_TOKEN_KEY);
     }
 }
