1e9b2c5ae7b25734f2b018e33aa4b8d1b774237c
Nicoooo26 <nicogpfutbol@gmail.com>
2023-05-09 08:49:33 +0200

    .

--- src/main/java/com/facturpdr/aplicacion/clientes/controladores/crearClienteControlador.java
+++ src/main/java/com/facturpdr/aplicacion/clientes/controladores/crearClienteControlador.java
@@ -5,7 +5,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 
-public class crear_clienteControlador {
+public class crearClienteControlador {
 
     @FXML
     public TextField textNombre;
