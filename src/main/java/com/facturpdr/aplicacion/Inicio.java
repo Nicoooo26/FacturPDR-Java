973026a8352768551fe5860889f2471964eed71e
raulsuper30 <rauldediego30@gmail.com>
2023-05-17 09:35:57 +0200

    Verificacion de conexion a Internet al iniciar

--- src/main/java/com/facturpdr/aplicacion/Inicio.java
+++ src/main/java/com/facturpdr/aplicacion/Inicio.java
@@ -2,6 +2,7 @@ package com.facturpdr.aplicacion;
 
 import com.facturpdr.aplicacion.general.utilidades.AlertaUtilidad;
 import com.facturpdr.aplicacion.general.utilidades.ConfiguracionUtilidad;
+import com.facturpdr.aplicacion.sesiones.utilidades.InternetUtilidad;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
@@ -11,6 +12,11 @@ public class Inicio extends Application {
         String sistema = System.getProperty("os.name");
         if (sistema.startsWith("Windows")) {
             AlertaUtilidad.error("Error de sistema operativo", "La aplicación solo se puede ejecutar en un sistema operativo Windows");
+            return;
+        }
+
+        if (!InternetUtilidad.estaConectado()) {
+            AlertaUtilidad.error("Error de conexión", "Actualmente nuestros servidores no esta disponibles, intentalo mas tarde");
             return;
         }
 
